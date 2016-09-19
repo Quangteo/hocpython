79e60ded33fc0097a2857297758cc0370e376b19
vinhquang-pyt <quangngovinh6189@gmail.com>
2016-09-19 23:29:29 +0700

    New

---
+++ tieusai.py
@@ -0,0 +1,12 @@
+l1 = list(range(0,20))
+for i in l1:
+    if i % 3 == 0:
+        print(i, 'fizz')
+    if i % 5 == 0:
+        print(i, 'buzz')
+    if i % 15 == 0:
+        print(i, 'fizzbuzz')
+else:
+    print('Khong co')
+
+#
